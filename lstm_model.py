00aa6b05d65d06e45b1b1e5da7c127edef615708
Денис <9112202@mail.ru>
2026-03-23 09:29:31 +0300

    Add files via upload

--- lstm_model.py
+++ lstm_model.py
@@ -14,16 +14,27 @@ class LSTMTextGenerator(nn.Module):
         output = self.fc(lstm_out)
         return output, (hidden, cell)
 
-    def generate(self, start_tokens, max_length, temperature=1.0, device='cuda', eos_token_id = 102):
+    def generate(self, start_tokens, max_length, temperature=1.0, device='cuda', eos_token_id=102):
+        self.eval()
         generated = start_tokens.clone().to(device)
-                
+
         with torch.no_grad():
-            for _ in range(max_length):
-                output, _ = self(generated)
-                next_token_logits = output[0, -1, :] / temperature
+            embedded = self.embedding(generated)
+            lstm_out, (hidden, cell) = self.lstm(embedded)
+            output = self.fc(lstm_out)
+            next_token_logits = output[0, -1, :] / temperature
+            next_token = torch.multinomial(torch.softmax(next_token_logits, dim=-1), 1)
+            if next_token.item() == eos_token_id:
+                return generated
+            generated = torch.cat([generated, next_token.unsqueeze(0)], dim=1)
+            for _ in range(max_length - 1):
+                last_token = generated[:, -1:]
+                embedded = self.embedding(last_token)
+                lstm_out, (hidden, cell) = self.lstm(embedded, (hidden, cell))
+                output = self.fc(lstm_out)
+                next_token_logits = output[0, 0, :] / temperature
                 next_token = torch.multinomial(torch.softmax(next_token_logits, dim=-1), 1)
                 if next_token.item() == eos_token_id:
                     break
-                else:    
-                    generated = torch.cat([generated, next_token.unsqueeze(0)], dim=1)
+                generated = torch.cat([generated, next_token], dim=1)
         return generated
